a2d4b509ad99251173d4d9a1b8b53b7774ca63e1
LLN247 <lum.nchifor@ictuniversity.edu.cm>
2025-06-04 16:10:17 +0100

    fix: update signupForm.jsx with resolved conflicts

--- src/pages/signupForm.jsx
+++ src/pages/signupForm.jsx
@@ -3,10 +3,11 @@ import FormComponent from "../components/Form";
 function SignupForm() {
   return (
     <FormComponent
+      type="signup"
       login_message="Welcome"
       form_title="Sign up"
       alternative_method="Login"
-      path={"/"}
+      path="/login"
     />
   );
 }
